cbfdf2c315e457116c920b4a39099bbac5dae4db
Huei-Horng Yo <hiroshi@hiroshi-archlinux.localdomain>
2011-03-07 10:13:59 +0800

    XML converter for Rarebooks to Digital Archives Union Catalog

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Rarecat::Application.config.secret_token = '9bee1ae3821b36b46b81ae6771f3d107a0497c7c8cca035de00e0fade57fecbcf7fdcfd64796406377357baa5682aa014bda4ea189b1750e2538cb770fe6d6f4'
