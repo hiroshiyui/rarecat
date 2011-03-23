2e26fb0c41ff02c13481350e5f7659cd53ae690f
Hiroshi Yui <hiroshi@gate.sinica.edu.tw>
2011-03-23 14:25:42 +0800

    Add queue listing helper.

--- app/helpers/rarebooks_xml_helper.rb
+++ app/helpers/rarebooks_xml_helper.rb
@@ -1,2 +1,10 @@
 module RarebooksXmlHelper
+  def list_queue(rarebooks)
+    if rarebooks.empty?
+      #render :text => "Empty queue."
+      render :inline => "<span>Empty queue.</span>"
+    else
+      render :layout => "queue"
+    end
+  end
 end
